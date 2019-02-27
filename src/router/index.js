aa213b159ed7358f0f2fb9c8055c5a1d39e8a356
yufeiqiang <l446731380@qq.com>
2019-02-27 14:45:51 +0800

    改了啊

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ router.beforeEach((to, from, next) => {
     iView.LoadingBar.start();
     // console.log(!Cookies.get('isLogin'))
     if (Cookies.get('isLogin')==530 && to.name !== LOGIN_PAGE_NAME) { // 判断是否已经登录且前往的页面不是登录页
-        alert('请登录啊啊啊啊')
+        alert('请登录啊')
         next({
             replace: true,
             name: 'login'   
